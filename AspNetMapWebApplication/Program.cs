b3b42e0efbfafa6c43552c261e2e1a39a3387c4e
maxefimov <efimov.top.academy@gmail.com>
2024-03-10 14:26:58 +0300

    10-03-24

--- AspNetMapWebApplication/Program.cs
+++ AspNetMapWebApplication/Program.cs
@@ -10,55 +10,55 @@ app.Map("/time", appBuilder =>
         Console.WriteLine($"Time: {time}");
         await next();
     });
-    appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
+    appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}\n"));
 });
 
-app.Map("/time", appBuilder =>
+app.MapWhen((context) => context.Request.Path == "/date", appBuilder =>
 {
-    var time = DateTime.Now.ToShortTimeString();
+    var date = DateTime.Now.ToShortDateString();
     appBuilder.Use(async (context, next) =>
     {
-        Console.WriteLine($"Time: {time}");
+        Console.WriteLine($"Date: {date}");
         await next();
     });
-    appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
+    appBuilder.Run(async context => await context.Response.WriteAsync($"Date: {date}\n"));
 });
 
-app.Run(async (context) => await context.Response.WriteAsync("Home page"));
+app.Run(async (context) => await context.Response.WriteAsync("Home page\n"));
 
 
-// 2
-//app.Map("/index", appBuilder =>
-//{
-//    appBuilder.Run(async context => await context.Response.WriteAsync("Index Page"));
-//});
-//app.Map("/about", appBuilder =>
-//{
-//    appBuilder.Run(async context => await context.Response.WriteAsync("About Page"));
-//});
+2
+app.Map("/index", appBuilder =>
+{
+    appBuilder.Run(async context => await context.Response.WriteAsync("Index Page"));
+});
+app.Map("/about", appBuilder =>
+{
+    appBuilder.Run(async context => await context.Response.WriteAsync("About Page"));
+});
 
-//app.Run(async (context) => await context.Response.WriteAsync("Page Not Found"));
+app.Run(async (context) => await context.Response.WriteAsync("Page Not Found"));
 
 //3
-//app.Map("/home", appBuilder =>
-//{
-//    appBuilder.Map("/index", Index); // middleware для "/home/index"
-//    appBuilder.Map("/about", About); // middleware для "/home/about"
-//    // middleware для "/home"
-//    appBuilder.Run(async (context) => await context.Response.WriteAsync("Home Page"));
-//});
+app.Map("/home", appBuilder =>
+{
+    appBuilder.Map("/index", Index); // middleware для "/home/index"
+    appBuilder.Map("/about", About); // middleware для "/home/about"
+    // middleware для "/home"
+    appBuilder.Run(async (context) => await context.Response.WriteAsync("Home Page"));
+});
 
 //app.Run(async (context) => await context.Response.WriteAsync("Page Not Found"));
 
 app.Run();
 
 
-// 3
-//void Index(IApplicationBuilder appBuilder)
-//{
-//    appBuilder.Run(async context => await context.Response.WriteAsync("Index Page"));
-//}
-//void About(IApplicationBuilder appBuilder)
-//{
-//    appBuilder.Run(async context => await context.Response.WriteAsync("About Page"));
-//}
+3
+void Index(IApplicationBuilder appBuilder)
+{
+    appBuilder.Run(async context => await context.Response.WriteAsync("Index Page"));
+}
+void About(IApplicationBuilder appBuilder)
+{
+    appBuilder.Run(async context => await context.Response.WriteAsync("About Page"));
+}
